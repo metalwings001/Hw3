71ab5ad56366af61435f4d8b198757dd242ffdec
metalwings001 <coolguyme5@gmail.com>
2020-02-24 15:43:46 -0800

    3

--- prob1.c
+++ prob1.c
@@ -3,8 +3,15 @@
 #include <unistd.h> 
 #include <pthread.h>
 
-int main() {
-   // printf() displays the string inside quotation
+void *thread(void *vargp) {
    printf("Hello, World!\n");
+}
+int main() {
+   pthread_t tid;
+
+   Pthread_create(&tid, NULL, thread, NULL);
+   Pthread_join(tid,NULL);
+   exit(0);
    return 0;
 }
+
