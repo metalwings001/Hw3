a7f74589c663b334612a5a13fb3e747df4f6b75b
metalwings001 <coolguyme5@gmail.com>
2020-02-24 15:25:34 -0800

    1

---
+++ prob1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <pthread.h>
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
