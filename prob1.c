43f91bc820b9752dea18ac6704adbb5ab2c2f3de
metalwings001 <coolguyme5@gmail.com>
2020-02-24 17:24:08 -0800

    6

--- prob1.c
+++ prob1.c
@@ -9,14 +9,15 @@ void *thread(void *vargp) {
 }
 int main(int argc, char* argv[]) {
    pthread_t tid;
-
    int numThreads;
-   if(argv[0] != '\0') {
-      numThreads = atoi(argv[0]);
+   
+   if(argv[1] != '\0') {
+      numThreads = atoi(argv[1]);
       printf("numThreads: %d\n",numThreads);
    }
-
-   pthread_create(&tid, NULL, thread, NULL);
+   for(int i = 0; i < numThreads; ++i){
+      pthread_create(&tid, NULL, thread, NULL);
+   }
    pthread_join(tid,NULL);
    exit(0);
    return 0;
