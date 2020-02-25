88e3bcf61a91cca32356b03cb5b82976c7f96b4d
metalwings001 <coolguyme5@gmail.com>
2020-02-24 17:20:21 -0800

    5

--- prob1.c
+++ prob1.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <unistd.h> 
+#include <string.h>
 #include <pthread.h>
 
 void *thread(void *vargp) {
    printf("Hello, World!\n");
 }
-int main() {
+int main(int argc, char* argv[]) {
    pthread_t tid;
+
+   int numThreads;
+   if(argv[0] != '\0') {
+      numThreads = atoi(argv[0]);
+      printf("numThreads: %d\n",numThreads);
+   }
 
    pthread_create(&tid, NULL, thread, NULL);
    pthread_join(tid,NULL);
